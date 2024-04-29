c108014ca28166734eb9c0541b7504d1ffed8b92
dibya.borah <dibyajyotiborah469@gmail.com>
2024-04-29 23:00:43 +0530

    counterSlice.actions are made , exported and called

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
-import {useSelector} from "react-redux";
-
+import { useDispatch, useSelector } from "react-redux";
+import { increment, reset, decrement } from "./features/counter/counterSlice";
 const App = () => {
-  const {count} = useSelector((state)=>{
+  const { count } = useSelector((state) => {
     return state.counter;
-  })
-  console.log(count);
+  });
+
+  const dispatch = useDispatch();
   return (
     <div>
       <h1>Count: {count}</h1>
-      <button>Increase</button>
-      <button>Reset</button>
-      <button>Decrease</button>
+      {
+        // ya pe hum state ko change karenge
+        // And hamesa yad rakhna
+        // Kisi v state ko change karne ke liye hum use karte hain dispatch function
+      }
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={() => {
+          dispatch(reset());
+        }}
+      >
+        Reset
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        Decrease
+      </button>
     </div>
   );
 };
 
 export default App;
+
+// dispatch function (action) ---> reducer --> Change state --> UI update
--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -2,15 +2,36 @@ import { createSlice } from "@reduxjs/toolkit";
 
 // Abhi counter feature ke related koi v code ho
 // For ex. Increment , reset or decrease
-// So sara ka sara code hum is feature ki matlab counter feature ki 
+// So sara ka sara code hum is feature ki matlab counter feature ki
 // SLICE main likhte hain
 
-const initialState = {count: 0};
+// reducer are responsible for updating the state
+
+const initialState = { count: 0 };
 
 const counterSlice = createSlice({
-    name: "counter",// ye hum ko redux internally use karti hain
-    initialState: initialState,// slice main hum state pass karte hain
+  name: "counter", // ye hum ko redux internally use karti hain
+  initialState: initialState, // slice main hum state pass karte hain
+  reducers: {
+    increment: (state) => {// ye yesa return kana implicitly (internally) handle karta hain  IMMER.js
+        // TO HUM NAHI V KARE to chalega
+        return {count: state.count + 1};// yaha pe + karke = karna padega tab state update hogi
+    },
+    decrement: (state) => {
+        state.count -= 1// here we are mutating an existing state 
+    },
+    reset: (state) => {
+        state.count = 0// we are not returning a new state as we do with useState
+        // redux internally return a new state by conversing the current state;
+    },
+  },
 });
+
+// CounterSlice (jo Slice hum create karte hain)
+// uske pas reducer v hota hain aur action v
+// So we do both .reducer and .actions
+
+export const {increment, reset, decrement} = counterSlice.actions;
 
 const counterReducer = counterSlice.reducer;
 
